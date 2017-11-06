e010e3ec4e3d2d3e26287a766a2b1e040d9a37b1
Jimmy LEFEVRE <j.lefevre@ceat-electronique.fr>
2017-11-06 12:05:10 +0100

    nodejs setup fix with npm added

--- nodejs/setup_node.sh
+++ nodejs/setup_node.sh
@@ -1,2 +1,2 @@
  #!/bin/bash
-sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install nodejs -y
+sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install nodejs npm -y
